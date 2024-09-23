522ef154c009bda3b8ddaff53e8b1ebd2dd235ca
Mohinurrrrr <m.xolmatova@newuu.uz>
2024-09-23 20:10:47 +0500

    Add files via upload

---
+++ problem4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n1,n2,n3,n4;
+    cin>>n1>>n2>>n3>>n4;
+    if ((n1==n2)&&(n2==n3)&&(n3==n4)) cout<<3;
+    else if ((n1==n2)&&(n1==n3)) cout<<2;
+    else if ((n1==n2)&&(n1==n4)) cout<<2;
+    else if ((n1==n3)&&(n3==n4)) cout<<2;
+    else if ((n2==n3)&&(n3==n4)) cout<<2;
+    else if (n1==n2) cout<<1;
+    else if (n1==n3) cout<<1;
+    else if (n1==n4) cout<<1;
+    else if (n2==n3) cout<<1;
+    else if (n2==n4) cout<<1;
+    else if (n3==n4) cout<<1;
+    else  cout<<0;
+
+
+
+    return 0;
+}
